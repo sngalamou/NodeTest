d48c8a1cb0ca731914e8ee5e1208f72fffb9cd1c
Simeon Ngalamou <simeonngalamou@lewisu.edu>
2022-02-21 18:12:35 -0600

    node assignment portfolio

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# node.js-kk
-Deployment of a Node.js backed website for Sprint 3 in Software Engineering 
+# NodeAssignmentPortfolio
--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-var http = require('http');
-var url = require('url');
-var dt = require('./datetime');
+const express = require('express');
+const path = require('path');
 
+const app = express();
 
-const server = http.createServer((request, response) => {
-    // Write the request to the log. 
-    console.log(request);
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+  
 
-    // Standard Hello World.
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h3>Hello World!</h3>')
+const PORT = process.env.PORT || 5000;
 
-    // Access funcion from a separate JavaScript module.
-    response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-    // Show the url. 
-    response.write("req.url="+request.url+"<br><br>");
-
-    // Suggest adding something tl the url so that we can parse it. 
-    response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
-    var q = url.parse(request.url, true).query;
-    var txt = q.year + " " + q.month;
-    response.write("txt="+txt);
-
-    // Close the response
-    response.end('<h3>The End.</h3>');
-});
-
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
